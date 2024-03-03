407e10f8a04528556b5d60d635ae5b958647775a
Shmuel Lamm <shmuellamm@gmail.com>
2024-03-03 22:19:27 +0200

    .

--- src/services/ai/runAgent.ts
+++ src/services/ai/runAgent.ts
@@ -7,19 +7,7 @@ import {
 } from '@langchain/core/callbacks/manager';
 import { Client } from 'langsmith';
 import { awaitAllCallbacks } from '@langchain/core/callbacks/promises';
-
-const makeId = (length: number) => {
-  let result = '';
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result;
-};
+import { makeId } from './utils/makeId.js';
 
 const AGENT_PROMPT = `You are a test agent, who is to follow the instructions so we can stress test our LLM logging.
 
