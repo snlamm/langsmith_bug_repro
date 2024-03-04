be8f6cbacde9814ae7106d7cefa0b7c468404e65
Shmuel Lamm <shmuellamm@gmail.com>
2024-03-04 09:47:18 +0200

    fail state

--- src/services/ai/runPrompts.ts
+++ src/services/ai/runPrompts.ts
@@ -1,5 +1,9 @@
 import { ChatOpenAI } from '@langchain/openai';
-import { traceAsGroup } from '@langchain/core/callbacks/manager';
+import {
+  CallbackManager,
+  Callbacks,
+  traceAsGroup,
+} from '@langchain/core/callbacks/manager';
 import { Client } from 'langsmith';
 import { LangChainTracer } from '@langchain/core/tracers/tracer_langchain';
 import { makeId } from './utils/makeId.js';
@@ -16,34 +20,59 @@ const getChatLLM = ({ shortId }: { shortId: string }) => {
   return chatLLM;
 };
 
+const getLangsmithClient = () => {
+  return new Client({
+    apiKey: process.env.LANGCHAIN_API_KEY,
+  });
+};
+
+const createLog = async ({
+  name,
+  shortId,
+  callbacks,
+}: {
+  name: string;
+  shortId: string;
+  callbacks: Callbacks;
+}) => {
+  const chatLLM = new ChatOpenAI({
+    modelName: 'gpt-3.5-turbo',
+    temperature: 0.4,
+    maxTokens: 500,
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    tags: [shortId],
+  });
+  const result = await chatLLM.invoke("Say the word 'test'", {
+    runName: `Log Test - ${name}`,
+    tags: [shortId],
+    callbacks,
+  });
+
+  return result;
+};
+
 export const runAllPrompts = async () => {
   try {
     const shortId = `testShortId:${makeId(5)}`;
 
     console.log(`starting! ShortId ${shortId}`);
 
-    const client = new Client({
-      apiKey: process.env.LANGCHAIN_API_KEY,
-    });
-
-    const chatLLM = getChatLLM({ shortId });
-
-    // this log works!
-    await chatLLM.invoke("Say the word 'hello'", {
-      runName: 'Log Test',
-      tags: [shortId],
+    const result = await createLog({
+      name: 'Succeeds',
+      shortId,
       callbacks: [
         new LangChainTracer({
-          client,
+          client: getLangsmithClient(),
           projectName: process.env.LANGSMITH_PROJECT_NAME,
         }),
       ],
     });
+    console.log('first result!', result);
 
     await traceAsGroup(
       {
-        name: 'Trace Group',
-        client,
+        name: `Trace Group ${Date.now()}`,
+        client: getLangsmithClient(),
         projectName: process.env.LANGSMITH_PROJECT_NAME,
       },
       async (callbackManager) => {
