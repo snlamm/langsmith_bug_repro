0e774b55dd18a6be82d259669e404282a64362d4
Shmuel Lamm <shmuellamm@gmail.com>
2024-03-04 10:46:17 +0200

    .

--- src/services/ai/runPrompts.ts
+++ src/services/ai/runPrompts.ts
@@ -1,42 +1,13 @@
 import { ChatOpenAI } from '@langchain/openai';
-import { Callbacks, traceAsGroup } from '@langchain/core/callbacks/manager';
+import { traceAsGroup } from '@langchain/core/callbacks/manager';
 import { Client } from 'langsmith';
 import { LangChainTracer } from '@langchain/core/tracers/tracer_langchain';
 import { makeId } from './utils/makeId.js';
-import {
-  BaseCallbackHandler,
-  CallbackHandlerMethods,
-} from '@langchain/core/dist/callbacks/base.js';
 
 const getLangsmithClient = () => {
   return new Client({
     apiKey: process.env.LANGCHAIN_API_KEY,
   });
-};
-
-const createLog = async ({
-  name,
-  shortId,
-  callbacks,
-}: {
-  name: string;
-  shortId: string;
-  callbacks: Callbacks;
-}) => {
-  const chatLLM = new ChatOpenAI({
-    modelName: 'gpt-3.5-turbo',
-    temperature: 0.4,
-    maxTokens: 500,
-    openAIApiKey: process.env.OPENAI_API_KEY,
-    tags: [shortId],
-  });
-  const result = await chatLLM.invoke("Say the word 'test'", {
-    runName: `Log Test - ${name}`,
-    tags: [shortId],
-    callbacks,
-  });
-
-  return result;
 };
 
 export const runAllPrompts = async () => {
